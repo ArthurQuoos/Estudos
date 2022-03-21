724e5a5c378f35bc74e79625596a848abef6afc8
ArthurQuoos <93408764+ArthurQuoos@users.noreply.github.com>
2022-03-21 09:00:18 -0300

    Update Vetor.s

--- ARCO1/Vetor.s
+++ ARCO1/Vetor.s
@@ -1,9 +1,10 @@
-
 .data
   str: .asciiz "Valor nao encontrado \n"
   vetor: .word 1, 2, 3, 4, 5, -1 , -2, -3, -100
-
-
+  bemvindo: .asciiz "Insira um valor: \n"
+  str2: .asciiz "Valor "
+  str3: .asciiz " encontrado\n"
+  
 .text
 
   main:
@@ -11,6 +12,10 @@
    li $t5, -100     #$t5 = -100
    
    la $s0, vetor    #$s0 = vetor[]
+   
+   li $v0, 4
+   la $a0 bemvindo
+   syscall
    
    li $v0, 5        #$v0 = var
    syscall
@@ -26,16 +31,25 @@
          j while
          
   imprimir:
+  
+  	 li $v0, 4          #imprimir a string2
+ 	 la $a0, str2
+ 	 syscall
+  
   	 li $v0, 1
   	 move $a0, $t3
   	 syscall
+  	 
+  	 li $v0, 4          #imprimir a string3
+ 	 la $a0, str3
+ 	 syscall
   	 
   	 li $v0, 10
   	 syscall
   	 
  fim:
  
- 	 li $v0, 4
+ 	 li $v0, 4          #imprimir a string
  	 la $a0, str
  	 syscall
  	 
