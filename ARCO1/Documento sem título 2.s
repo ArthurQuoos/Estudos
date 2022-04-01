33d1ceae9b7c33f89f9f07a415e90afd95ae7aa0
ArthurQuoos <93408764+ArthurQuoos@users.noreply.github.com>
2022-04-01 01:53:42 -0300

    Update Documento sem título 2.s

--- ARCO1/Documento sem título 2.s
+++ ARCO1/Documento sem título 2.s
@@ -13,23 +13,23 @@
     
     main:
     
-    addi $t1, $zero, 3
-    addi $t2, $zero, 5
-    addi $t4, $zero, 10
+    addi $t1, $zero, 3    #
+    addi $t2, $zero, 5    #
+    addi $t4, $zero, 10   #
     
-    add $t4, $zero, $t1
+    add $t4, $zero, $t1   #
     
-    slt $t1,$t2,$t4
-    beq $t1,$t4,fim
+    slt $t1,$t2,$t4       #
+    beq $t1,$t4,fim       #
     
     
-    add $t4, $zero, $t2
+    add $t4, $zero, $t2   #
    
    fim:
    
-   li $v0, 1
-   add $a0, $zero, $t4
-   syscall
+   li $v0, 1               #
+   add $a0, $zero, $t4     #
+   syscall                 #
    
     
     
