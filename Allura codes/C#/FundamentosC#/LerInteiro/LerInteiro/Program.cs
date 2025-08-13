fe5f6d08ecab245f6f9c9121044e209fab302778
ArthurQuoos <93408764+ArthurQuoos@users.noreply.github.com>
2025-08-12 21:12:49 -0300

    Add files via upload

---
+++ Allura codes/C#/FundamentosC#/LerInteiro/LerInteiro/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace EntradasUteis
+{
+    using System;
+
+    public static class EntradasUteis
+    {
+        public static int LerInteiro(string mensagem)
+        {
+            int numero;
+            string entrada;
+
+            do
+            {
+                Console.WriteLine(mensagem);
+                entrada = Console.ReadLine();
+
+                if (!int.TryParse(entrada, out numero))
+                {
+                    Console.WriteLine("Entrada inválida. Tente novamente.\n");
+                }
+
+            } while (!int.TryParse(entrada, out numero));
+
+            return numero;
+        }
+
+        public static float LerFloat(string mensagem)
+        {
+            float numero;
+            string entrada;
+
+            do
+            {
+                Console.WriteLine(mensagem);
+                entrada = Console.ReadLine();
+
+                if (!float.TryParse(entrada, out numero))
+                {
+                    Console.WriteLine("Entrada inválida. Tente novamente.\n");
+                }
+
+            } while (!float.TryParse(entrada, out numero));
+
+            return numero;
+        }
+    }
+}
