c53a160d25e6cce4488354e049e0e36b8c318b81
ArthurQuoos <93408764+ArthurQuoos@users.noreply.github.com>
2025-09-24 09:03:42 -0300

    Update MemCompartilhada.c

--- Sistemas Operacionais/execl/MemCompartilhada.c
+++ Sistemas Operacionais/execl/MemCompartilhada.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/shm.h>
+#include <string.h>
 
 //gcc -o ex exemplo.c
 // ./ex "Valor"
@@ -9,18 +12,18 @@ void main(int argc,char *argv[]){
     char *buff;
     int key = ftok(".",'A');// Criando chave: H:\Sistemas Operacionais\execl
     int shmid = shmget(key, 50*sizeof(char), IPC_CREAT | 0644);
-    buff = shamt(shmid,0,0);
+    buff = shmat(shmid,0,0);
 
 if((argv[1] != NULL) && (strcmp(argv[1], "apagar"))){
     strcpy(buff,argv[1]);
+
 }else if((argc > 1 )&& (!strcmp(argv[1], "apagar"))){
     //strcpy(buff, " ");
     shmdt(buff); //desanexando regiao de memoria
     shmctl(shmid, IPC_RMID, NULL);  //destruindo regiao de memoria
+
 }else{
     printf("->> %s \n", buff);
 }
-
-
 
 }
