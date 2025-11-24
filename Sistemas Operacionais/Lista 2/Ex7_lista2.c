c79d90e0e829b6709a9bbd4ad25f97d127c90fdc
ArthurQuoos <93408764+ArthurQuoos@users.noreply.github.com>
2025-11-24 20:07:39 -0300

    Implement multi-threaded row sum for matrix
    This program creates N threads to sum the values of each row in a matrix and calculates the total sum of the matrix. It includes necessary synchronization mechanisms to handle potential issues.

---
+++ Sistemas Operacionais/Lista 2/Ex7_lista2.c
@@ -0,0 +1,80 @@
+/*
+7) programa abaixo manipula uma matriz de tamanho MxN (veja os defines para o tamanho da matriz). A função SumValues soma todos os valores em uma linha da matriz. A linha a ser somada é
+identificada pela variável i. Modifique o programa principal (main) nos locais indicados para:
+
+
+           1. Criar N threads, uma para somar os valores de cada linha.
+
+           2. Receber o resultado do somatório de cada linha e gerar o somatório total da matriz.
+
+           3. Analise o programa: há problemas de sincronização que precisam ser resolvidos? Se sim, resolva-os.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+/* number of matrix columns and rows */
+#define M 5
+#define N 10
+ 
+int matrix[N][M];
+pthread_t threads[N];
+int sum_total;
+
+
+/* thread function; it sums the values of the matrix in the row */
+void* SumValues(void* i)
+{
+    int n = (long) i;     /* row number */
+    int j, total = 0;
+
+    /* sum values of row n */
+    for (j = 0; j < M; j++)
+        total += matrix[n][j];
+
+    printf("The total in row %d is %d\n", n, total);
+
+    /* allocate space to return the result */
+    int* retval = malloc(sizeof(int));
+    *retval = total;
+
+    pthread_exit(retval);
+}
+
+
+int main(int argc, char *argv[])
+{
+    sum_total = 0;
+    int i, j;
+
+    /* initialize the matrix */
+    for (i = 0; i < N; i++)
+        for (j = 0; j < M; j++)
+            matrix[i][j] = i * M + j;
+
+    /* ----------------------------------------- */
+    /* 1. CREATE THREADS                         */
+    /* ----------------------------------------- */
+    for (i = 0; i < N; i++)
+        pthread_create(&threads[i], NULL, SumValues, (void*)(long)i);
+
+      
+    /* ----------------------------------------- */
+    /* 2. WAIT THREADS AND SUM RESULTS           */
+    /* ----------------------------------------- */
+    for (i = 0; i < N; i++) {
+        int *retval;
+        pthread_join(threads[i], (void**)&retval);
+
+        sum_total += *retval;   // add row sum to total
+
+        free(retval);           // free memory returned by the thread
+    }
+ 
+    printf("The total values in the matrix is %d\n", sum_total);
+
+    return 0;
+}
